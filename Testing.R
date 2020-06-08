f1d0e52350e82c57858a6b9d354aef048ce576ff
LeeJinSheng <johnshenglee1996@gmail.com>
2020-06-08 16:03:13 +0800

    initial commit

---
+++ Testing.R
@@ -0,0 +1,9 @@
+print("This file was created within RStudio")
+print("And now it lives on GitHub"
+print("This file was created within RStudio")
+print("And now it lives on GitHub")
+print("This file was created within RStudio")
+print("And now it lives on GitHub")
+source('~/RStudio Script/testing/Testing.R')
+source('~/RStudio Script/testing/Testing.R')
+savehistory("~/RStudio Script/testing/Testing.R")
